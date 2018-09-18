10bd9116fdafc082b7811f103ff90b7787ac0d28
HiRoS Neko <hiros.neko@gmail.com>
2018-09-17 22:24:19 -0400

    Initial Commit

---
+++ Assets/Insect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.Networking;
+
+public class Insect : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rgd;
+    [SerializeField, Range(0, 5)] private float _speed;
+
+    [SerializeField, Range(0, 10)] private float _maxVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_rgd == null) _rgd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Move(Vector3 dir, float rot)
+    {
+        //Add force to insect
+        _rgd.AddForce(dir * _speed, ForceMode.VelocityChange);
+        //Ensure velocity isn't greater than max velocity
+        if (_rgd.velocity.magnitude > _maxVelocity) _rgd.velocity = _rgd.velocity.normalized * _maxVelocity;
+        _rgd.rotation = Quaternion.Euler((_rgd.rotation.eulerAngles.y + rot) * Vector3.up);
+    }
+
+    public void Jump(float force)
+    {
+        _rgd.AddRelativeForce(Vector3.up * force);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField, Range(1, 5)] private float _cameraSens;
+    
+    [SerializeField] private Insect _playerObj;
+
+    [SerializeField] private Camera _camera;
+
+    private Vector3 _dir;
+
+    private float _rot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _dir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+
+        _rot = Input.GetAxis("Mouse X") * _cameraSens;
+
+        _camera.transform.rotation =
+            Quaternion.Euler(_camera.transform.rotation.eulerAngles + Input.GetAxis("Mouse Y") * _cameraSens * Vector3.right);
+
+        if (Input.GetButtonDown("Jump")) _playerObj.Jump(5);
+    }
+
+    private void FixedUpdate()
+    {
+        _playerObj.Move(_dir, _rot);
+    }
+}
